9ba31590eb195a8d4c9eda598c81032046ad9ce8
LucasCubile <cubilelucas@gmail.com>
2026-01-16 00:36:09 -0300

    Refactor mongoose connection setup
    Refactor database connection logic to use connectDB function.

--- back/src/db.ts
+++ back/src/db.ts
@@ -1,8 +1,11 @@
-import {connect} from "mongoose";
-import {mongodb} from "./keys"
+import mongoose from "mongoose";
+import { mongodb } from "./keys";
 
-(async () => {
-    connect(mongodb.URI) 
-.then(_db => console.log(`db is connected`))
-.catch(error => console.log(error))
-})()
+mongoose.set("bufferCommands", false);
+
+export const connectDB = () => {
+  return mongoose.connect(mongodb.URI, {
+    serverSelectionTimeoutMS: 30000,
+  });
+};
+
